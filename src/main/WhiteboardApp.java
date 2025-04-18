8c2e9394421d42f4f092ae38dcf1f1a4e1b44de2
Luis Mauboy <mago@Luiss-MacBook-Air.local>
2025-04-18 12:40:49 +1000

    first commit

---
+++ src/main/WhiteboardApp.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Event.*;
+import java.util.ArrayList;
+
+public class WhiteboardApp {
+	public static void main(String args[]) {
+		SwingUtilities.invokeLater(() -> new WhiteboardFrame().setVisible(true));
+	}
+}
+
+class WhiteboardFrame extends JFrame {
+	
+}
